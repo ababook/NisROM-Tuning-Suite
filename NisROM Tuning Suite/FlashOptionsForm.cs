2be7aa9c50f885d8d3957942df2b54e973b670b2
Evan <esanders@protonmail.com>
2017-06-05 22:00:14 -0600

    added crc correction for flashing

---
+++ NisROM Tuning Suite/FlashOptionsForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NisROM_Tuning_Suite
+{
+    public partial class FlashOptionsForm : Form
+    {
+        public FlashOptionsForm()
+        {
+            InitializeComponent();
+        }
+
+        private UInt16 CRC16(byte[] packet, int length)
+        {
+            const UInt16 num = 0x8408;
+            UInt16 crc = 0xffff;
+            for(int i = 0; i < length; i++)
+            {
+                crc ^= packet[i];
+                for(int j = 0; j < 8; j++)
+                {
+                    if((crc & 0x0001) > 0)
+                    {
+                        crc >>= 1;
+                        crc ^= num;
+                    }
+                    else
+                    {
+                        crc >>= 1;
+                    }
+                }
+            }
+            return crc;
+        }
+    }
+}
