48ddea9a28ad09c1fd5df57c83207e3319cf8ae7
Evan <esanders@protonmail.com>
2017-06-06 23:06:29 -0600

    initial commit for serial communication

---
+++ NisROM Tuning Suite/Serial/SerialManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace NisROM_Tuning_Suite.Serial
+{
+    public class SerialManager
+    {
+        private SerialPort port;
+
+        public SerialManager()
+        {
+            port = new SerialPort();
+        }
+    }
+}
