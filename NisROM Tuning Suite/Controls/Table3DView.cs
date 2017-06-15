8a7acc2d37a10d022c6642557dac7f1bd624e1e4
Evan <esanders@protonmail.com>
2017-06-15 17:12:52 -0600

    fixed bug with table loading/saving

--- NisROM Tuning Suite/Controls/Table3DView.cs
+++ NisROM Tuning Suite/Controls/Table3DView.cs
@@ -121,6 +121,37 @@ namespace NisROM_Tuning_Suite.Controls
                     dataGridView1.Rows.Add(row);
                 }
             }
+            else
+            {
+                byte[] table8 = new byte[256];
+                for (uint i = tableAddr; i < (tableAddr + 256); i++)
+                {
+                    table8[i - tableAddr] = MainForm.ecuRom.RomBytes[i];
+                }
+                for (int i = 0; i < 16; i++)
+                {
+                    DataGridViewRow row = (DataGridViewRow)dataGridView1.RowTemplate.Clone();
+                    row.CreateCells(dataGridView1,
+                        ConvertFromExpression(table8[0 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[1 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[2 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[3 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[4 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[5 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[6 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[7 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[8 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[9 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[10 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[11 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[12 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[13 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[14 + (i * 16)], RomTable.Scaling.Expression),
+                        ConvertFromExpression(table8[15 + (i * 16)], RomTable.Scaling.Expression)
+                        );
+                    dataGridView1.Rows.Add(row);
+                }
+            }
             if(RomTable.XAxis.StorageType == "uint16")
             {
                 xAx = new ushort[16];
@@ -158,7 +189,11 @@ namespace NisROM_Tuning_Suite.Controls
                 }
                 for (int i = 0; i < 16; i++)
                 {
-                    dataGridView1.Rows[i].HeaderCell.Value = ConvertFromExpression(yAx[i], RomTable.YAxis.Scaling.Expression).ToString();
+                    try
+                    {
+                        dataGridView1.Rows[i].HeaderCell.Value = ConvertFromExpression(yAx[i], RomTable.YAxis.Scaling.Expression).ToString();
+                    }
+                    catch { }
                 }
             }
             else
@@ -170,7 +205,11 @@ namespace NisROM_Tuning_Suite.Controls
                 }
                 for (int i = 0; i < 16; i++)
                 {
-                    dataGridView1.Rows[i].HeaderCell.Value = ConvertFromExpression(Ax8[i], RomTable.YAxis.Scaling.Expression).ToString();
+                    try
+                    {
+                        dataGridView1.Rows[i].HeaderCell.Value = ConvertFromExpression(Ax8[i], RomTable.YAxis.Scaling.Expression).ToString();
+                    }
+                    catch { }
                 }
             }
         }
@@ -277,7 +316,7 @@ namespace NisROM_Tuning_Suite.Controls
                     MainForm.ecuRom.RomBytes[i] = table[i - tableAddr];
                 }
             }
-            else
+            else if(RomTable.StorageType == "uint16")
             {
                 ushort[] table = new ushort[256];
                 int k = 0;
@@ -295,6 +334,23 @@ namespace NisROM_Tuning_Suite.Controls
                     MainForm.ecuRom.RomBytes[i] = (byte)(table[k] >> 8);
                     MainForm.ecuRom.RomBytes[i + 1] = (byte)(table[k]);
                     k++;
+                }
+            }
+            else
+            {
+                byte[] table = new byte[256];
+                int k = 0;
+                for (int i = 0; i < 16; i++)
+                {
+                    for (int j = 0; j < 16; j++)
+                    {
+                        table[k] = ConvertToRomByte(dataGridView1.Rows[i].Cells[j].Value.ToString(), RomTable.Scaling.To_Byte);
+                        k++;
+                    }
+                }
+                for (uint i = tableAddr; i < (tableAddr + 256); i++)
+                {
+                    MainForm.ecuRom.RomBytes[i] = table[i - tableAddr];
                 }
             }
         }
@@ -341,6 +397,51 @@ namespace NisROM_Tuning_Suite.Controls
             dataGridView1.CurrentCell.Value = newValue;
         }
 
+        private void LoadHeatmap()
+        {
+            List<double> values = new List<double>();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    values.Add(Convert.ToDouble(cell.Value));
+                }
+            }
+            if (values.Count() == 0) return;
+            double max = values.Max();
+            double min = values.Min();
+            double i1 = max - ((max - min) / 5);
+            double i2 = max - (((max - min) / 5) * 2);
+            double i3 = max - (((max - min) / 5) * 3);
+            double i4 = max - (((max - min) / 5) * 4);
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    double value = Convert.ToDouble(cell.Value);
+                    if (value > i1 && value <= max)
+                    {
+                        cell.Style.BackColor = heatMapColors[0];
+                    }
+                    else if (value > i2 && value <= i1)
+                    {
+                        cell.Style.BackColor = heatMapColors[1];
+                    }
+                    else if (value > i3 && value <= i2)
+                    {
+                        cell.Style.BackColor = heatMapColors[2];
+                    }
+                    else if (value > i4 && value <= i3)
+                    {
+                        cell.Style.BackColor = heatMapColors[3];
+                    }
+                    else
+                    {
+                        cell.Style.BackColor = heatMapColors[4];
+                    }
+                }
+            }
+        }
         private void Table3DView_Load(object sender, EventArgs e)
         {
             if(RomTable.SizeX == "8" && RomTable.SizeY == "8")
@@ -351,6 +452,7 @@ namespace NisROM_Tuning_Suite.Controls
             {
                 SetTable16x16();
             }
+            LoadHeatmap();
         }
 
         private void dataGridView_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
@@ -380,47 +482,7 @@ namespace NisROM_Tuning_Suite.Controls
 
         private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
-            List<double> values = new List<double>();
-            foreach(DataGridViewRow row in dataGridView1.Rows)
-            {
-                foreach(DataGridViewCell cell in row.Cells)
-                {
-                    values.Add(Convert.ToDouble(cell.Value));
-                }
-            }
-            double max = values.Max();
-            double min = values.Min();
-            double i1 = max - ((max - min) / 5);
-            double i2 = max - (((max - min) / 5) * 2);
-            double i3 = max - (((max - min) / 5) * 3);
-            double i4 = max - (((max - min) / 5) * 4);
-            foreach (DataGridViewRow row in dataGridView1.Rows)
-            {
-                foreach(DataGridViewCell cell in row.Cells)
-                {
-                    double value = Convert.ToDouble(cell.Value);
-                    if(value > i1 && value <= max)
-                    {
-                        cell.Style.BackColor = heatMapColors[0];
-                    }
-                    else if(value > i2 && value <= i1)
-                    {
-                        cell.Style.BackColor = heatMapColors[1];
-                    }
-                    else if(value > i3 && value <= i2)
-                    {
-                        cell.Style.BackColor = heatMapColors[2];
-                    }
-                    else if(value > i4 && value <= i3)
-                    {
-                        cell.Style.BackColor = heatMapColors[3];
-                    }
-                    else
-                    {
-                        cell.Style.BackColor = heatMapColors[4];
-                    }
-                }
-            }
+            LoadHeatmap();
         }
     }
 }
